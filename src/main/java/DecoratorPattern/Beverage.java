faab1e7680ddd5067003da35e0fd576f7a3f9bef
wesleynepo <wesleynepomuceno1@gmail.com>
2024-01-25 00:13:29 -0300

    fad

--- src/main/java/DecoratorPattern/Beverage.java
+++ src/main/java/DecoratorPattern/Beverage.java
@@ -2,9 +2,19 @@ package DecoratorPattern;
 
 public abstract class Beverage {
     String description = "Unknown beverage";
+    public enum Size { TALL, GRANDE, VENTI };
+    Size size = Size.TALL;
 
     public String getDescription() {
         return description;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Size getSize() {
+        return size;
     }
 
     public abstract double cost();
---
+++ src/main/java/DecoratorPattern/StarbuzzCoffee.java
@@ -0,0 +1,13 @@
+package DecoratorPattern;
+
+public class StarbuzzCoffee {
+    public static void main(String args[]) {
+        Beverage beverage = new Expresso();
+        System.out.println(beverage.getDescription() + beverage.cost());
+
+        Beverage beverage2 = new Expresso();
+        beverage2 = new Mocha(beverage2);
+
+        System.out.println(beverage2.getDescription() + beverage2.cost());
+    }
+}
